a1f3da4124246dba0b9362c045a06ea31fd053f5
swagatakar111 <swagatakarpersonal@gmail.com>
2025-12-12 22:54:33 +0530

    Company controller and API testing

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js";
+import companyRoute from "./routes/company.route.js";
 
 dotenv.config({});
 
@@ -21,7 +22,7 @@ app.use(cors(corsOptions));
 const PORT = process.env.PORT || 3000;
 
 app.use("/api/v1/user",userRoute);
-
+app.use("/api/v1/company",companyRoute);
 
 
 app.listen(PORT,()=>{
