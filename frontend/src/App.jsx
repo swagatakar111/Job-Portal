821d068cb2bdb2b038eef77f33ef84dbb4ff23e1
swagatakar111 <swagatakarpersonal@gmail.com>
2025-12-18 20:54:19 +0530

    Building Login/Signup Page and working on multer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,31 @@
 
-import Navbar from './components/shared/Navbar.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from './components/auth/Login.jsx'
+import Signup from './components/auth/Signup.jsx'
+import Home from './components/Home.jsx';
+import { Toaster } from "sonner";
+
+
+const appRouter = createBrowserRouter([
+  {
+    path:'/',
+    element:<Home/>
+  },
+    {
+    path:'/login',
+    element:<Login/>
+  },
+   {
+    path:'/signup',
+    element:<Signup/>
+  }
+])
 
 export default function App() {
   return (
     <>
-      <Navbar />
+    <Toaster richColors position="top-right" />
+      <RouterProvider router={appRouter}/>
     </>
   );
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,8 +6,6 @@ import App from "./App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
       <App />
-    </BrowserRouter>
   </StrictMode>
 );
