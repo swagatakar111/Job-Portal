a46014579c82cb65d1afd29114099fe3647c8c6a
swagatakar111 <swagatakarpersonal@gmail.com>
2025-12-28 15:17:34 +0530

    Made Job Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Login from './components/auth/Login.jsx'
 import Signup from './components/auth/Signup.jsx'
 import Home from './components/Home.jsx';
 import { Toaster } from "sonner";
+import Jobs from "./components/Jobs.jsx";
 
 
 const appRouter = createBrowserRouter([
@@ -18,6 +19,10 @@ const appRouter = createBrowserRouter([
    {
     path:'/signup',
     element:<Signup/>
+  },
+  {
+    path:'/jobs',
+    element:<Jobs/>
   }
 ])
 
